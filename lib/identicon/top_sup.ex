624d361fcf1579c7393218fc8c2146d4b27c5d70
RaymondLoranger <Raymond.Loranger@bell.net>
2023-02-02 19:54:20 -0500

    0.1.24

--- lib/identicon/top_sup.ex
+++ lib/identicon/top_sup.ex
@@ -4,7 +4,6 @@ defmodule Identicon.TopSup do
   alias __MODULE__
   alias Identicon.DirPath.Server
 
-  @impl Application
   @spec start(Application.start_type(), term) :: {:ok, pid}
   def start(_start_type, :ok = _start_args) do
     [
