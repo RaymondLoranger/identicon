9bc1176ea3af3d4217e4fa540d9a9cc7f9c67f17
RaymondLoranger <Raymond.Loranger@bell.net>
2023-11-29 10:12:24 -0500

    0.1.27

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Identicon.MixProject do
   def project do
     [
       app: :identicon,
-      version: "0.1.26",
+      version: "0.1.27",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps()
